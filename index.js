fb2d339aeb95d2673ad0e93d0e5a2b1c22492fb8
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2025-02-19 12:15:10 +0200

    Done

---
+++ index.js
@@ -0,0 +1,30 @@
+const farengate = 32;
+const celcium = 0;
+console.log(celcium + 32);
+
+const dayCount = 365;
+console.log(dayCount * 24);
+console.log(dayCount * 60);
+
+let charechterEnergy = 100;
+let charechterHealth = 20;
+console.log(charechterEnergy - 24);
+console.log(charechterHealth - 10);
+
+let purchaseAmount = 1000;
+let purchaseSale = 0.10;
+console.log(purchaseAmount * purchaseSale);
+
+console.log(Math.floor(1.7));
+
+console.log(Number.parseFloat(4.66));
+console.log(Number.parseInt(4.66));
+
+let num = 100;
+console.log(Math.sqrt(num));
+
+let numWhol = 19.3;
+console.log(Number.parseInt(numWhol));
+let toStringn = numWhol.toString();
+console.log(toStringn);
+
