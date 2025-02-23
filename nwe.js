44c166290d7e80b437e4bde92256c9c9f067cc2b
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2025-02-23 19:46:14 +0200

    Added backup

---
+++ nwe.js
@@ -0,0 +1,71 @@
+// const message = 'Mango ' + 'is' + ' happy';
+// console.log(message); 
+
+// const newMessage = 'Orange' + ' is not ' + 'fine';
+// console.log(newMessage);
+
+// let result;
+
+// result = 5 + 5 + '5';
+// console.log(result);
+
+// typeof result;
+// console.log(typeof result);
+
+// const message = 'Welcome to Dnipro!';
+// console.log(message + 'YEAH!!!!!');
+// console.log(message.length);
+// console.log(message.toUpperCase());
+// console.log(message.indexOf('to'));
+// console.log(message.indexOf('o'));
+// console.log(message.includes('Welcome'));
+// console.log(message.startsWith('Wel'));
+// console.log(message.endsWith('!'));
+
+// const name = 'Kirill';
+// const age = 14;
+// const lol = "lol";
+// const mood = 'happy';
+
+// const message =
+//      `My name is ` + name + ", I'm " + age + ` years old and ` + mood + '! ' + lol;
+
+// console.log(message);
+
+// const sameMessage = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+// console.log(sameMessage);
+
+// const message = prompt('Enter your name');
+// console.log(message.trim());
+// document.write(message);
+
+// const name = 'Гість ';
+// const insome = ' поселяється  ';
+// const hotel = ' в номер';
+// const x = ' x ';
+// const y = ' y ';
+// const n = ' n ';
+// const g = ' g ';
+// console.log(name + x + y + insome +  n + hotel + g);
+// console.log("© kirill 2025");
+
+// const name = prompt('name enter');
+// console.log(name.trim());
+
+// const orderMsg = `Ви замовляєте 15 тістечок.`;
+// console.log(orderMsg.includes(15));
+// console.log(orderMsg.indexOf('1'));
+// document.write(orderMsg);
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'розпродаж';
+
+
+const string1 = 'Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!';
+const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+const string3 = 'Рекламна кампанія #jsIsAwesome';
+console.log(string1.includes(blacklistedWord1));
+console.log(string2.includes(blacklistedWord2));
+// © KIRILL 2025, is u copy this code, u must to write the author of this code
+
+// console.log(name.indexOf('k'));
